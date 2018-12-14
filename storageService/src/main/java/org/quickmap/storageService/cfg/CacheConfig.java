98e287efc3fb4aa921c516255f5fc32a0a221fd0
lzh2016 <461698156@qq.com>
2018-12-14 20:21:26 +0800

    2018年12月14日20:21:20

---
+++ storageService/src/main/java/org/quickmap/storageService/cfg/CacheConfig.java
@@ -0,0 +1,13 @@
+package org.quickmap.storageService.cfg;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+/**
+ * 缓存配置
+ */
+public class CacheConfig {
+
+}
--- storageService/src/main/java/org/quickmap/storageService/dao/base/FileInfoBaseMapper.java
+++ storageService/src/main/java/org/quickmap/storageService/dao/base/FileInfoBaseMapper.java
@@ -1,21 +1,32 @@
 package org.quickmap.storageService.dao.base;
 
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 import org.quickmap.storageService.dao.model.FileInfo;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+
+import java.util.List;
+
 /**
 *  @author author
 */
+@CacheConfig(cacheNames = "fileInfos")
 public interface FileInfoBaseMapper {
 
+    @CachePut()
     int insertFileInfo(FileInfo object);
 
+    @CacheEvict(allEntries = true)
     int updateFileInfo(FileInfo object);
 
+    @CacheEvict(allEntries = true)
     int update(FileInfo.UpdateBuilder object);
 
+    @Cacheable()
     List<FileInfo> queryFileInfo(FileInfo object);
 
+    @Cacheable()
     FileInfo queryFileInfoLimit1(FileInfo object);
 
 }
