9196b735016abbd6f60ae880455fd0f870d9e072
lzh2016 <461698156@qq.com>
2018-12-14 20:35:12 +0800

    2018年12月14日20:21:20

--- storageService/src/main/java/org/quickmap/storageService/cfg/CacheConfig.java
+++ storageService/src/main/java/org/quickmap/storageService/cfg/CacheConfig.java
@@ -3,11 +3,11 @@ package org.quickmap.storageService.cfg;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableCaching
 /**
  * 缓存配置
  */
+@Configuration
+@EnableCaching
 public class CacheConfig {
 
 }
